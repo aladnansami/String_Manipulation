bfda5f4d1da95a4478b93e52d91db006eae4c51a
Al Adnan Sami <46723678+aladnansami@users.noreply.github.com>
2022-07-17 22:44:03 +0600

    Added

--- src/main/java/String_Manipulation/how_many_images.java
+++ src/main/java/String_Manipulation/how_many_images.java
@@ -5,4 +5,16 @@
 package String_Manipulation;
 
 public class how_many_images {
+    public static void main(String[] args) {
+        String[] photo = {"photo1.jpg", "doc1.pdf", "doc2.docx", "photo2.png", "doc3.pdf", "msoffice.exe", "photo3.jpg"};
+
+        int count = 0;
+
+        for( String output : photo){
+            if(output.contains(".jpg")  || output.contains(".png") || output.contains(".jpeg") ){
+                count += 1;
+            }
+        }
+        System.out.println("Total photo: " + count);
+    }
 }
