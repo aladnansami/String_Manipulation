1b6a159dc9964fcc5b6e4803de3d885efbba03db
Al Adnan Sami <46723678+aladnansami@users.noreply.github.com>
2022-07-23 18:39:59 +0600

    Added

--- src/main/java/String_Manipulation/TotalCost.java
+++ src/main/java/String_Manipulation/TotalCost.java
@@ -11,32 +11,19 @@ import java.util.Scanner;
 
 public class TotalCost {
     public static void main(String[] args) {
-        int formalShirt;
-        int shirtPrice = 1200;
-        int sharee;
-        int shareePrice= 3500;
-        int discountPrice= 400;
+        String str = "Price of a formal shirt is 1200 tk and Sharee is 3500 tk. If you buy 2 shirt and 1 sharee, then 400 tk will be discounted. After your purchase what will be your total cost?";
+        str = str.replaceAll("[^\\d]", " ");
+        str = str.trim();
+        str = str.replaceAll(" +", " ");
+        String[] arr = (str.split(" "));
 
-        System.out.println("shirtPrice = 1200");
-        System.out.println("shareePrice= 3500");
-        System.out.println("if you buy two shirt and one sharee you will get 400 tk discount");
-        System.out.println("Enter Your Amount of shirt");
-        Scanner scanner= new Scanner(System.in);
-        formalShirt=scanner.nextInt();
-        System.out.println("Enter Your Amount of shirt");
-        Scanner scanner2= new Scanner(System.in);
-        sharee=scanner.nextInt();
+        int shirt_price = Integer.parseInt(arr[0]);
+        int sharee_price = Integer.parseInt(arr[1]);
+        int shirt_quantity = Integer.parseInt(arr[2]);
+        int sharee_quantity = Integer.parseInt(arr[3]);
+        int discount = Integer.parseInt(arr[4]);
+        int total_price = (shirt_price * shirt_quantity) + (sharee_price * sharee_quantity) - discount;
+        System.out.println("Total cost " + total_price);
 
-        int Total = formalShirt*shirtPrice + sharee*shareePrice;
-        if (formalShirt==2 & sharee==1){
-            int TotalDiscount = Total-discountPrice;
-            System.out.println("Price after Discount " + TotalDiscount);
-        }
-        else {
-            System.out.println("Price Without Discount "+Total);
-        }
-    scanner.close();
-        scanner2.close();
     }
-
 }
